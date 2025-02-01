fc987ae0009332a76428435439e74fa2a05fa8dc
frinklx <160569799+frinklx@users.noreply.github.com>
2025-02-01 12:48:36 +0800

    fix

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,10 +1,10 @@
 const config = {
   development: {
-    DISCORD_CLIENT_ID: "1335084588212551702",
-    API_ENDPOINT: "http://localhost:3001/api",
-    WS_ENDPOINT: "ws://localhost:3001/ws",
+    DISCORD_CLIENT_ID: "1199445891861975040",
+    API_ENDPOINT: "http://localhost:8080/api",
+    WS_ENDPOINT: "ws://localhost:8080/ws",
     YOUR_USER_ID: "1241605075047153776",
-    REDIRECT_URI: "http://localhost:3001/dashboard/callback",
+    REDIRECT_URI: "http://localhost:8080/dashboard/callback",
   },
   production: {
     DISCORD_CLIENT_ID: "1335084588212551702",
